df9bd5d141aa93a27215a6ddda2efc4f3d8b9be2
Arcadiy Ivanov <arcadiy@ivanov.biz>
2024-08-08 23:22:06 -0400

    Release 0.0.8

--- build.py
+++ build.py
@@ -30,7 +30,7 @@ use_plugin("copy_resources")
 use_plugin("filter_resources")
 
 name = "wheel_axle"
-version = "0.0.8.dev"
+version = "0.0.8"
 
 summary = "Axle is Python wheel enhancement library"
 authors = [Author("Karellen, Inc.", "supervisor@karellen.co")]
